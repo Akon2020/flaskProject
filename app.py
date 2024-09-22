a88a1328f09244fe0fe12a17693f03a5d3af3cd7
Akonkwa Ushindi Isaac <akonkwaushindi@gmail.com>
2024-09-22 17:43:26 +0200

    404 Page

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 from flask import Flask, render_template
 
-# Initialiser l'application Flask
 app = Flask(__name__)
 
-# Route pour la page d'accueil
 @app.route('/')
 def home():
-    return "Bienvenue dans mon projet Flask !"
-
-@app.route('/home')
-def dash():
     return render_template('index.html')
 
-# Lancer l'application Flask
+@app.errorhandler(404)
+def pagenotfound(error):
+    return render_template('404.html'), 404
+
 if __name__ == '__main__':
     app.run(debug=True)
