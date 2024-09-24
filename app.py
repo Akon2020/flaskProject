2bfdaf2ff5b1410aa95b8792508c656608659c7b
Akonkwa Ushindi Isaac <akonkwaushindi@gmail.com>
2024-09-24 17:22:35 +0200

    topnav adapted

--- app.py
+++ app.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 @app.errorhandler(404)
 def pagenotfound(error):
     return render_template('404.html'), 404
