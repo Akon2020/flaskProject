16cb4e03ced3037e6682d1ccd673681127d145a8
Akonkwa Ushindi Isaac <akonkwaushindi@gmail.com>
2024-09-24 18:33:28 +0200

    Popular products

--- static/js/demo/chart-pie-demo.js
+++ static/js/demo/chart-pie-demo.js
@@ -1,26 +1,47 @@
 // Set new default font family and font color to mimic Bootstrap's default styling
-Chart.defaults.global.defaultFontFamily = 'Nunito', '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
-Chart.defaults.global.defaultFontColor = '#858796';
+(Chart.defaults.global.defaultFontFamily = "Nunito"),
+  '-apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif';
+Chart.defaults.global.defaultFontColor = "#858796";
 
 // Pie Chart Example
 var ctx = document.getElementById("myPieChart");
 var myPieChart = new Chart(ctx, {
-  type: 'doughnut',
+  type: "doughnut",
   data: {
-    labels: ["Direct", "Referral", "Social"],
-    datasets: [{
-      data: [55, 30, 15],
-      backgroundColor: ['#4e73df', '#1cc88a', '#36b9cc'],
-      hoverBackgroundColor: ['#2e59d9', '#17a673', '#2c9faf'],
-      hoverBorderColor: "rgba(234, 236, 244, 1)",
-    }],
+    labels: [
+      "Amortisseur",
+      "Pompe",
+      "Batterie",
+      "Pare-brise",
+      "Huile de moteur",
+    ],
+    datasets: [
+      {
+        data: [46, 20, 13, 9, 12],
+        backgroundColor: [
+          "#4e73df",
+          "#1cc88a",
+          "#36b9cc",
+          "#f6c23e",
+          "#e74a3b",
+        ],
+        hoverBackgroundColor: [
+          "#2e59d9",
+          "#17a673",
+          "#2c9faf",
+          "#f1b22a",
+          "#d6393b",
+        ],
+        hoverBorderColor: "rgba(234, 236, 244, 1)",
+      },
+    ],
   },
   options: {
     maintainAspectRatio: false,
     tooltips: {
       backgroundColor: "rgb(255,255,255)",
       bodyFontColor: "#858796",
-      borderColor: '#dddfeb',
+      borderColor: "#dddfeb",
       borderWidth: 1,
       xPadding: 15,
       yPadding: 15,
@@ -28,7 +49,7 @@ var myPieChart = new Chart(ctx, {
       caretPadding: 10,
     },
     legend: {
-      display: false
+      display: false,
     },
     cutoutPercentage: 80,
   },
