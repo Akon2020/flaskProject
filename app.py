fb6b3a149bc5c4b4def64361f972fce5a69f3fd6
Akonkwa Ushindi Isaac <akonkwaushindi@gmail.com>
2024-10-22 17:52:20 +0200

    First test with visualisation

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+from werkzeug.utils import secure_filename
+import io
+import base64
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads/'
+app.config['ALLOWED_EXTENSIONS'] = {'csv'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
 
 @app.route('/')
 def home():
@@ -10,8 +24,26 @@ def home():
 def dashboard():
     return render_template('index.html')
 
-@app.route('/info')
+@app.route('/info', methods=['GET', 'POST'])
 def info():
+    if request.method == 'POST':
+        titre = request.form['titre']
+        fichier = request.files['document']
+        if fichier and allowed_file(fichier.filename):
+            filename = secure_filename(fichier.filename)
+            cheminFichier = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            fichier.save(cheminFichier)
+            data = pd.read_csv(cheminFichier)
+            tables = data.to_html(classes='table table-striped', header="true")
+            fig, ax = plt.subplots()
+            data.plot(ax=ax)
+            img = io.BytesIO()
+            plt.savefig(img, format='png')
+            img.seek(0)
+            plot_url = base64.b64encode(img.getvalue()).decode('utf8')
+
+            return render_template('utilite.html', titre=titre, tables=tables, plot_url=f'data:image/png;base64,{plot_url}')
+
     return render_template('utilite.html')
 
 @app.route('/about')
